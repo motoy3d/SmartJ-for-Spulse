7299a49111644fce23fb78edc6a867dc239cbdd3
motoy3d <motoy3d@gmail.com>
2014-12-15 04:12:43 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'spulse'
+    ,hashtag: 'spulse'
+    ,teamName: '清水'
+    ,appName: 'スマートJ for 清水エスパルス'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id920789827?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.spulse'
+    ,color: '#f57b00'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.spulse'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=spulse&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=spulse"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 236832
+    ,nendApiKeyIPhoneIcon: 'ccce7289b472a877b1d58c43311ad24c307a4be6'
+    ,nendSpotIdIPhoneBanner: 236831
+    ,nendApiKeyIPhoneBanner: 'bbd82805d1279f9bd0ec372e0b13464d68e5270f'
+    //nend(Android)
+    ,nendSpotIdAndroid: 236830
+    ,nendApiKeyAndroid: 'a09d69e8e966b3afdb0dd36ea9cbd44460e4e7eb'
+};
